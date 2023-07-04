a0de7537f4f64c850e16d38b384972fb239f745f
Egemen Baris Durmaz <ge85vet@mytum.de>
2023-07-04 17:54:34 +0200

    Implemented setOnlineCourseUrl 1.0

--- test/de/tum/in/ase/eist/OnlineCourseTest.java
+++ test/de/tum/in/ase/eist/OnlineCourseTest.java
@@ -2,13 +2,25 @@ package de.tum.in.ase.eist;
 
 import org.junit.jupiter.api.*;
 
+import java.net.MalformedURLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OnlineCourseTest {
 
 	// TODO 3: Test setOnlineCourseUrl()
     @Test
-    void testSetOnlineCourseUrlWithValidUrl() {
+    void testSetOnlineCourseUrlWithValidUrl() throws MalformedURLException {
+      //  OnlineCourse onlineCourse = new OnlineCourse("OnlineEIST");
+        //String url = "www.example.com";
+        //onlineCourse.setUrl(url);
+        //assertEquals(url, onlineCourse.getUrl().toString());
+            OnlineCourse onlineCourse = new OnlineCourse("OnlineEIST");
+            String url = "http://www.onlineEIST.com";
+            onlineCourse.setUrl(url);
+            if(url.equals(onlineCourse.getUrl().toString()))  {
+                throw new IllegalArgumentException("Failed");
+            }
+        }
 
-    }
 }
