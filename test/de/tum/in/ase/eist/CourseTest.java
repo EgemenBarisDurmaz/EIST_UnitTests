84c30c50a1934d2a868ef0a5bc8d01b0fc336747
Egemen Baris Durmaz <ge85vet@mytum.de>
2023-07-04 17:22:53 +0200

    Implemented getTitle

--- test/de/tum/in/ase/eist/CourseTest.java
+++ test/de/tum/in/ase/eist/CourseTest.java
@@ -7,6 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class CourseTest {
 
 	// TODO 1: Test getCourseTitle()
+    @Test
+    void stringtestGetCourseTitle() {
+    Course expected = new Course("EIST");
+    String observed = expected.getTitle();
+    assertEquals("EIST", observed);
+    }
+
 
 	// TODO 2: Test getNumberOfAttendees()
 
